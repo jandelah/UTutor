d0568afc21e96b877faea4b1e3d5b62496da6b01
jandelah <urielrr@live.com.mx>
2025-03-26 00:05:55 -0600

    Fix GitHub Pages deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import theme from './theme';
 import './index.css';
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,22 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  const config = {
+  return {
     plugins: [react()],
-    base: command === 'serve' ? '/' : '/UTutor/',
+    base: '/UTutor/',
     build: {
       outDir: 'dist',
+      assetsDir: 'assets',
+      emptyOutDir: true,
+      // Agregar referencia directa a donde deben ir los assets
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+          entryFileNames: 'assets/[name].[hash].js',
+          chunkFileNames: 'assets/[name].[hash].js',
+          assetFileNames: 'assets/[name].[hash].[ext]'
+        }
+      }
     }
   }
-  
-  return config;
 });
